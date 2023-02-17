07cc90c37a5df34cebb93ac968c97d3df5e6fe26
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-02-17 13:11:40 +0200

    Create Movies.jsx

---
+++ src/pages/Movies/Movies.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { getQueryMovies } from 'services/moviesApi';
+
+import Loader from 'components/Loader/Loader';
+import { Notify } from 'notiflix';
+
+import MoviesList from 'components/MoviesList/MoviesList';
+
+const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const search = searchParams.get('search');
+
+  useEffect(() => {
+    if (search === '' || search === null) {
+      return;
+    }
+
+    const fetchQueryMovies = async () => {
+      try {
+        setLoading(true);
+        const data = await getQueryMovies(search);
+        setMovies(data.results);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchQueryMovies();
+  }, [search]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    setSearchParams({ search: form.elements.search.value });
+    form.reset();
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search"
+          required
+          autoComplete="off"
+          autoFocus
+          placeholder="Search movies..."
+        />
+        <button type="submit">Search</button>
+      </form>
+      {loading && <Loader />}
+      {error
+        ? Notify.failure('Oop! Something went wrong! Try again later!')
+        : movies && <MoviesList movies={movies} />}
+    </>
+  );
+};
+
+export default Movies;
