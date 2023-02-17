69f46be016a30d9870181145584b904c7d0e08a4
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-02-17 13:11:21 +0200

    Create Home.jsx

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import TrendingList from "components/TrendingList/TrendingList"
+
+const Home = () => {
+    return (
+        <>
+            <h1>Trending today</h1>
+            <TrendingList/>
+        </>
+    )
+}
+
+export default Home;
