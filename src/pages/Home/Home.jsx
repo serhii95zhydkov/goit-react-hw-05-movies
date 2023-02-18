2d34af692dbd74e7cc9d7ec4d930fff8e1cea783
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-02-18 12:51:00 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
-import TrendingList from "components/TrendingList/TrendingList"
+import TrendingList from 'components/TrendingList/TrendingList';
+
+import styles from './home.module.css';
 
 const Home = () => {
-    return (
-        <>
-            <h1>Trending today</h1>
-            <TrendingList/>
-        </>
-    )
-}
+  return (
+    <>
+      <h1 className={styles.title}>Trending today</h1>
+      <TrendingList />
+    </>
+  );
+};
 
 export default Home;
