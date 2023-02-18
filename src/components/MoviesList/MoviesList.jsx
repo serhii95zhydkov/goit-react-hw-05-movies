53866b73728adb397fcdd8926f7acaf1efbff078
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-02-18 12:51:15 +0200

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,13 +1,26 @@
 import { useLocation, Link } from 'react-router-dom';
 
+import PropTypes from 'prop-types';
+
+import styles from './moviesList.module.css';
+
 const MoviesList = ({ movies }) => {
   const location = useLocation();
 
-  const elements = movies.map(({ title, id }) => {
+  const baseImgUrl = 'https://image.tmdb.org/t/p/w500';
+
+  const elements = movies.map(({ title, id, poster_path }) => {
     return (
       <li key={id}>
-        <Link to={`/movies/${id}`} state={{ from: location }}>
-          {title}
+        <Link
+          className={styles.link}
+          to={`/movies/${id}`}
+          state={{ from: location }}
+        >
+          <div className={styles.container}>
+            <img src={`${baseImgUrl}${poster_path}`} alt={title} width={220} />
+            <p className={styles.text}>{title}</p>
+          </div>
         </Link>
       </li>
     );
@@ -15,9 +28,19 @@ const MoviesList = ({ movies }) => {
 
   return (
     <>
-      <ul>{elements}</ul>
+      <ul className={styles.list}>{elements}</ul>
     </>
   );
 };
 
 export default MoviesList;
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      poster_path: PropTypes.string,
+    })
+  ).isRequired,
+};
